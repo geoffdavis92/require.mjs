1f3d46c30ed9073f9066132beb2e41531d8af4f6
Geoff Davis <geoffdavis92@gmail.com>
2017-11-12 17:37:53 -0600

    fn seems to work? adding to npm to test

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import fs from "fs";
 
-const require = (filepath: string, encoding: string = "utf8"): Object =>
-	JSON.parse(fs.readFileSync(filepath, { encoding }));
+const require = (filepath: string, encoding: string = "utf8"): Object => {
+	return JSON.parse(fs.readFileSync(filepath, { encoding }));
+};
 
 export default require;
